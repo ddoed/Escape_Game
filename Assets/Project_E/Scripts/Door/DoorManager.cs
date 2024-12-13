08c2bf1ac4dbb7fbee7fe294b12e94fd5d927039
ddoed <yimnabin20051022@gmail.com>
2024-12-13 12:20:54 +0900

    pc 작업용

---
+++ Assets/Project_E/Scripts/Door/DoorManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    public Door[] doorEntities;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CheckDoorId(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CheckDoorId(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            CheckDoorId(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            CheckDoorId(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            CheckDoorId(4);
+        }
+    }
+
+    public void CheckDoorId(int id)
+    {
+        foreach (Door door in doorEntities)
+        {
+            if (door.id != id)
+                continue;
+
+            door.OpenDoor();
+        }
+    }
+}
